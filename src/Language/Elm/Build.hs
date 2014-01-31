bfc66b8ad4ef2ca5ffdfaa19b7061177f8284d9e
Joey Eremondi <jse313@mail.usask.ca>
2014-01-30 22:36:02 -0600

    Clean implementation of library

--- src/Language/Elm/Build.hs
+++ src/Language/Elm/Build.hs
@@ -1,35 +1,98 @@
-module Language.Elm.Build where
+{-# LANGUAGE OverloadedStrings #-}
 
-import System.Cmd
-import System.Directory
-import qualified System.IO.Temp as Temp
-
-
-
--- |Wrapper type for an Elm module
-newtype Module = Module {moduleToString :: String}
-
-type Javascript = String
-
-
--- | Write the given Elm module strings to a temp directory
--- and compile them using Elm, returning the resulting JavaScript
-buildModules :: [(String, Module)] -> String ->  IO Javascript
-buildModules fileModuleList mainModule = Temp.withTempDirectory "" ".elm_temp" (\dir -> do
-  mapM_ writeElmSource fileModuleList
-  cd <- getCurrentDirectory
-  setCurrentDirectory dir
-
-  _exitCode <- system $ "elm --make --only-js " ++ mainModule ++ ".elm"
+module Language.Elm.Build (
+  Module,
+  Javascript,
+  BuildOptions(..),
+  ModuleName,
+  ModuleSource,
+  defaultOptions,
+  moduleFromString,
+  moduleFromFile,
+  buildModules,
+  buildModulesWithOptions
   
-  let outputPath = dir ++ "/build/" ++ mainModule
+  ) where
+
+import System.Process (readProcessWithExitCode)
+import System.Directory (doesFileExist)
+import System.Exit (ExitCode(..))
+import Data.Maybe (catMaybes, fromMaybe)
+import System.IO.Temp (withTempDirectory)
+import Data.Text
+import qualified Data.Text.IO as TextIO
+
+
+type ModuleName = Text
+
+type ModuleSource = Text
+
+newtype InternalModule = Module (ModuleName, ModuleSource)
+
+type Module = InternalModule
+
+type Javascript = Text
+
+-- | Abstraction for the options given to the elm executable
+-- Note that not all Elm options may be avaliable
+data BuildOptions = BuildOptions {
+    elmBinPath :: Maybe String,
+    elmRuntimePath :: Maybe String,
+    makeHtml :: Bool
+  }
+
+-- Default options are: `elm` as binary, no runtime given, and generate JS only
+defaultOptions :: BuildOptions
+defaultOptions = BuildOptions {
+ elmBinPath = Nothing,
+ elmRuntimePath = Nothing,
+ makeHtml = False
+}
+
+-- | Generate a module with the given Module name (e.g. 'MyLib.Foo')
+-- and the given source code
+moduleFromString :: ModuleName -> ModuleSource -> Module
+moduleFromString name source = Module (name, source)
+
+-- | Read a module from a file, with the given module name and file path
+moduleFromFile :: ModuleName -> FilePath -> IO Module
+moduleFromFile name path = do
+  src <- TextIO.readFile path
+  return $ moduleFromString name src
+
+-- | Build a group of elm modules with the `elm` from the system `$PATH`
+-- generating JavaScript using the default runtime location
+buildModules ::  Module -> [Module]  ->  IO (Either String Javascript)
+buildModules = buildModulesWithOptions defaultOptions
   
-  retJS <- readFile outputPath
+-- | Given an elm "main" module, and a list of other modules,
+-- compile them using the `--make` option and the given options
+buildModulesWithOptions :: BuildOptions -> Module -> [Module]  -> IO (Either String Javascript)
+buildModulesWithOptions options mainModule@(Module (mainName, _)) otherModules = withTempDirectory "" ".elm_temp" (\dir -> do
+  mapM_ writeElmSource otherModules
+  writeElmSource mainModule
+
+  let binPath = fromMaybe "elm" $ elmBinPath options
+  let runtimeOption = maybe Nothing (\path -> Just $ "--runtime=" ++ path) $ elmRuntimePath options
+  let genJSOption = if (makeHtml options) then Nothing else (Just "--only-js")
+  let resultExt = if (makeHtml options) then ".html" else ".js"
   
-  setCurrentDirectory cd
+  let cmdlineOptions = ["--make",  "--build-dir=" ++ dir, "--cache-dir=" ++ dir ++"/cache"] ++ catMaybes [runtimeOption, genJSOption] ++ [unpack mainName ++ ".elm"]
   
-  return retJS
+  (exitCode, stdout, stderr) <- readProcessWithExitCode binPath cmdlineOptions []
   
+  case exitCode of
+       ExitFailure i -> return $ Left $ "Elm failed with exit code " ++ (show i) ++ " and errors:" ++ stdout ++ stderr
+       _ -> do
+          putStrLn $ stdout ++ stderr
+          let outputPath = dir ++ "/" ++ unpack mainName ++ resultExt
+          exists <- doesFileExist outputPath
+          case exists of
+               False -> return $ Left "Could not find output file from Elm"
+               _ -> do
+                  retJS <- TextIO.readFile outputPath
+                  return $ Right retJS
+    
   )
   where
-    writeElmSource (path, Module source) = writeFile path source
+    writeElmSource (Module (moduleName, source)) = TextIO.writeFile (unpack moduleName ++ ".elm") source
